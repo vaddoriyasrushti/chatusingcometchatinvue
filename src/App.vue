5ad97c2525698ebab534ac8104fc0f0fd3504940
Sweta Mistry <vaddoriyasrushti9259@gmail.com>
2019-08-26 11:37:27 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { CometChat } from "@cometchat-pro/chat";
+
+import "./App.css";
+
+export default {
+  created() {
+    this.initializeApp();
+  },
+
+  methods: {
+    initializeApp() {
+      const { VUE_APP_COMMETCHAT_APP_ID } = process.env
+      CometChat.init(VUE_APP_COMMETCHAT_APP_ID).then(
+        () => {
+          console.log("Initialization completed successfully");
+        },
+        error => {
+          console.log("Initialization failed with error:", error);
+        }
+      );
+    }
+  }
+};
+</script>
